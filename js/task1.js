13c3c3813c251749030dc9f0bd569f019277d7a8
karina.yesbukenova <karina.yesbukenova@gmail.com>
2022-02-13 18:29:02 +0600

    second task

--- js/task1.js
+++ js/task1.js
@@ -2,3 +2,12 @@
 
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "")
 console.log(numberOfFilms)
+
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false,
+};
