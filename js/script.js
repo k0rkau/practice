ad9392de41849e47b9632951efae8244605a05d5
karina.yesbukenova <karina.yesbukenova@gmail.com>
2022-02-13 17:57:23 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+let number = 4.6;
+
+console.log(-4/0);
+console.log("Iska" * 9);
+
+
+const person = 'Karina';
+console.log(person);
+
+
+const da = true;
+const net = false;
+
+
+let und;
+console.log(und);
+
+const obj = {
+    name: "Karina",
+    age: 25,
+    isMarried: true
+};
+
+// console.log(obj.name)
+console.log(obj['name']);
