590c2f7cd69eb6e8cfe138577be38f7180784e50
karina.yesbukenova <karina.yesbukenova@gmail.com>
2022-02-14 23:05:16 +0600

    third task of 2nd practice

--- js/tasks/task2.js
+++ js/tasks/task2.js
@@ -10,22 +10,32 @@ let personalMovieDB = {
     private: false,
 };
 
-for (let i = 1; i <= numberOfFilms; i++) {
-    let lastViewdFilm = prompt("Один из последних просмотренных фильмов?", "");
-    let rate = +prompt("На сколько оцените его?", "");
-
-    if (lastViewdFilm == '' || rate == '') {
-        alert('Нельзя вводить пустую строку.');
-        i = 1;
-    } else if (lastViewdFilm === null || rate === null) {
-        alert('Нельзя отменять ввод.');
-        i = 1;
-    } else if (lastViewdFilm.length > 50) {
-        alert('Название фильма не должно превышать 50 символов.');
-        i = 1;
-    } 
-
-    personalMovieDB.movies[lastViewdFilm] = rate;
+if (numberOfFilms < 10) {
+    alert('Просмотрено довольно мало фильмов.');
+} else if (numberOfFilms >= 10 && numberOfFilms <=30) {
+    alert('Вы классический зритель.');
+} else if (numberOfFilms > 30) {
+    alert('Вы киноман.');
+} else {
+    alert('Произошла ошибка!');
 }
 
-console.log(personalMovieDB);
+// for (let i = 1; i <= numberOfFilms; i++) {
+//     let lastViewdFilm = prompt("Один из последних просмотренных фильмов?", "");
+//     let rate = +prompt("На сколько оцените его?", "");
+
+//     if (lastViewdFilm == '' || rate == '') {
+//         alert('Нельзя вводить пустую строку.');
+//         i = 1;
+//     } else if (lastViewdFilm === null || rate === null) {
+//         alert('Нельзя отменять ввод.');
+//         i = 1;
+//     } else if (lastViewdFilm.length > 50) {
+//         alert('Название фильма не должно превышать 50 символов.');
+//         i = 1;
+//     } 
+
+//     personalMovieDB.movies[lastViewdFilm] = rate;
+// }
+
+// console.log(personalMovieDB);
